9d2051632d0ffb4ef50346e57f06f0d725c65cef
Vishal <vishalsharma05052002@gmail.com>
2025-03-18 23:57:14 +0530

    basic changes

--- app.py
+++ app.py
@@ -121,4 +121,5 @@ def save_to_database():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+   port = int(os.environ.get("PORT", 8000))  # Default to 8000 if PORT not set
+   app.run(host="0.0.0.0", port=port)
